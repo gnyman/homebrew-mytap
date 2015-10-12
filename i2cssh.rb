1367c48578359b8b887a21961d929a799264aeb8
Gabriel N <gabriel@walkbase.com>
2015-10-12 16:11:07 +0200

    Fix url

--- i2cssh.rb
+++ i2cssh.rb
@@ -1,7 +1,7 @@
 class I2cssh < Formula
   homepage "https://github.com/djui/i2cssh"
   head "https://github.com/djui/i2cssh.git"
-  url "https://github.com/djui/i2cssh/archive/1.14.0.tar.gz"
+  url "https://github.com/djui/i2cssh/archive/v1.14.0.tar.gz"
   sha256 "5ff5732f7ae86549b9b09f285c65a36e541cfac1806b69df3192f03af449f1d8"
 
   def install
