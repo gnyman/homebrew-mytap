3539598cde36b4e8e4ad6b37b842cca744a65305
Gabriel N <gabriel@walkbase.com>
2015-10-12 16:06:02 +0200

    Initial commit based on https://github.com/Homebrew/homebrew/pull/38229

---
+++ Library/Formula/i2cssh.rb
@@ -0,0 +1,14 @@
+class I2cssh < Formula
+  homepage "https://github.com/djui/i2cssh"
+  head "https://github.com/djui/i2cssh.git"
+  url "https://github.com/djui/i2cssh/archive/1.14.0.tar.gz"
+  sha256 "5ff5732f7ae86549b9b09f285c65a36e541cfac1806b69df3192f03af449f1d8"
+
+  def install
+    bin.install "i2cssh"
+  end
+
+  test do
+    system "#{bin}/i2cssh"
+  end
+end
